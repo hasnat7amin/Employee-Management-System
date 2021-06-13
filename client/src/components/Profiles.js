9608a2f960c59d494098004749de3c9cdc49f96f
Hasnat Amin <hasnatamin065@gmail.com>
2021-06-13 13:04:38 +0500

    updating files

--- client/src/components/Profiles.js
+++ client/src/components/Profiles.js
@@ -67,8 +67,7 @@ function Profiles() {
                
               </div>
               <div class='container'></div>
-              <table  id="dtHorizontalExample" style="overflow-x:auto; width:400px; position:absolute " class="table table-striped table-bordered table-sm" cellspacing="1"
-  width="100%">
+              <table  id="dtHorizontalExample"  class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
                 <thead className="thead-dark">
                   <tr>
                     <th scope="col">Name</th>
