b7945d1a7c1f480859b2b3cdfe5c52c43dc7fc45
Hasnat Amin <hasnatamin065@gmail.com>
2021-06-13 12:39:03 +0500

    updating files

--- client/src/components/Profiles.js
+++ client/src/components/Profiles.js
@@ -50,12 +50,12 @@ function Profiles() {
               <p className="lead text-center">List of employee details</p>
               <div className='container m-4' >
                 <div class="row">
-                  <div class="col-md-6">
+                  <div class="col-md-6  col-sm-12">
                     <Link to='/addemployee'>
                       <button className='btn btn-dark'>Add New Employee</button>
                     </Link>
                     </div>
-                    <div class="col-md-6 mx-2">
+                    <div class="col-md-6 col-sm-12 my-2">
                     <SearchBar
                         value={search}
                         placeholder="Search An Employee With Name"
