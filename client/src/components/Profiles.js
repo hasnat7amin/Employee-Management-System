e8dd66fa593f0cf23e45cf4c3d2c21397dc08484
Hasnat Amin <hasnatamin065@gmail.com>
2021-06-13 13:23:33 +0500

    updating files

--- client/src/components/Profiles.js
+++ client/src/components/Profiles.js
@@ -67,7 +67,7 @@ function Profiles() {
                
               </div>
               <div class='container'></div>
-              <table  id="dtHorizontalExample"  class="table table-striped table-hover table-responsive" cellspacing="0" width="100%">
+              <table  id="dtHorizontalExample"  class="table table-striped table-bordered table-hover table-xl table-responsive" width="100%">
                 <thead className="thead-dark">
                   <tr>
                     <th scope="col">Name</th>
