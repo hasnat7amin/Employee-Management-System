bd02c70fcfa5332458d606faed13749858ad03a2
Hasnat Amin <hasnatamin065@gmail.com>
2021-06-13 13:44:56 +0500

    updating files

--- client/src/components/Profiles.js
+++ client/src/components/Profiles.js
@@ -55,7 +55,7 @@ function Profiles() {
                       <button className='btn btn-dark'>Add New Employee</button>
                     </Link>
                     </div>
-                    <div class="col-md-6 m-2">
+                    <div class="col-md-6 my-2">
                     <SearchBar
                         value={search}
                         placeholder="Search An Employee With Name"
